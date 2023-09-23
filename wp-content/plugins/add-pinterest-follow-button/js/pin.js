cf1b25cc47efa9efd408b4692cac948e5dbbe2f1
meneerdames <85730883+meneerdames@users.noreply.github.com>
2023-09-23 12:24:40 +0200

    Initial static site added to project.

---
+++ wp-content/plugins/add-pinterest-follow-button/js/pin.js
@@ -0,0 +1,8 @@
+(function ($) {
+  jQuery(document).ready(function () {
+    if (jQuery("span").parents("#pinterest_large")) {
+      var count = jQuery("#pinterest_large").addClass("pinterest_large_btn");
+      x = count;
+    }
+  });
+})(jQuery);
